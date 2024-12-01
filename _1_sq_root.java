7824894459c2c2caddcded7cddf70bfbe1def9a0
DEBJIT <0508debjitkundu2005@gmail.com>
2024-12-01 11:56:28 +0530

    sq root fn updated

--- _1_sq_root.java
+++ _1_sq_root.java
@@ -1,3 +1,38 @@
-public class _1_sq_root {
-    
+import java.util.Scanner;
+public class _1_sq_root{
+    public static void root(double x){
+        double val = 0.0;
+        while ((val+1)*(val+1)<=x){
+            val+=1;
+        }
+        int c= 0;
+        double diff=1.0;
+        while (c<20){
+            if ((val+diff/2)*(val+diff/2)<=x){
+                val+=diff/2;
+            }
+            diff/=2;
+            c++;
+        }
+
+        System.out.println("The square root of "+x+" is "+val);
+
+    }
+
+    public static void main(String [] args){
+        try(Scanner sc = new Scanner(System.in);) {
+            
+
+            System.out.println("Enter a number to find te root.");
+            double num = sc.nextDouble();
+            if (num<0){
+                System.out.println("The square root of a negaive number is undefined in real world ");
+            }
+            else{
+                root(num);
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        }
 }
